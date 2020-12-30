2f4b1f2a23d430a88bd0b35a1a51a6d609a69768
Zeryab Khan <51020585+izeryab@users.noreply.github.com>
2020-12-30 19:23:50 +0500

    Add files via upload

---
+++ CODEANALYZERMASTER/CODEANALYZERMASTER/Result.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CODEANALYZERMASTER
+{
+    class Result
+    {
+        public int LOC { get; set; }
+        public int SCLOC { get; set; }
+        public int CLOC { get; set; }
+        public int NCLOC { get; set; }
+        public int BLOC { get; set; }
+        public int LLOC { get; set; }
+        
+    }
+}
---
+++ CODEANALYZERMASTER/CODEANALYZERMASTER/SCLOC.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CODEANALYZERMASTER
+{
+    interface SCLOC
+    {
+        int Lines { get; set; }
+        void OnCodeLine(string line);
+        void Reset();
+    }
+    interface ILineFilter
+    {
+        bool MatchCondition(string line);
+    }
+}
